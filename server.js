888dce0701327ba6d602fd1f2943d0f848db9eda
Lâm Cương Đạt <lam.cuong.dat@gmail.com>
2021-04-09 21:48:50 +0700

    Update to deploy on Heroku:

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4200
+// Start the app by listening on the default Heroku port
+app.listen(port, () => {
+  console.log(`Anki Crawler is listening on port ${port}`)
+});
+
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/anki-crawler'));
+
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname+'/anki-crawler/index.html'));
+});
